2a27ad9c7fbb4da3a0853e2cc392b50f90f51198
AnonymouZEE <130084491+AnonymouZEE@users.noreply.github.com>
2026-04-15 16:26:13 +0100

    feat: Improve i18n and manifest
    Adds dynamic language updates based on i18n settings and includes additional manifest icons for better PWA support. Also sets `dir="ltr"` on the html tag for better text directionality.

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,24 @@
-import {StrictMode} from 'react';
+import {StrictMode, useEffect} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import './i18n';
+import i18n from './i18n';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+function Root() {
+  useEffect(() => {
+    const updateLang = () => {
+      document.documentElement.lang = i18n.language;
+    };
+    i18n.on('languageChanged', updateLang);
+    updateLang();
+    return () => i18n.off('languageChanged', updateLang);
+  }, []);
+
+  return (
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+}
+
+createRoot(document.getElementById('root')!).render(<Root />);
